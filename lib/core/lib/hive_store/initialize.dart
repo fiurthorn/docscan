5faa5b9584945fec5763cc8781241b935ea90e2a
Stefan Weinmann <s.weinmann@intex-online.de>
2023-08-15 08:05:11 +0200

    feature: update save configuration

--- lib/core/lib/hive_store/initialize.dart
+++ lib/core/lib/hive_store/initialize.dart
@@ -3,9 +3,12 @@ import 'package:document_scanner/core/lib/logger.dart';
 import 'package:document_scanner/core/lib/trace.dart';
 import 'package:document_scanner/core/platform/platform.dart';
 import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future<Box<dynamic>> initHive() async {
-  if (isDesktop || (isMobile && !isWeb)) Hive.init(trace(await appDocumentsDir(), "appDocumentsDir"));
+  if (isDesktop || (isMobile && !isWeb)) {
+    Hive.init(trace(await getLibraryDirectory().then((dir) => dir.path), "getLibraryDirectory"));
+  }
 
   Hive.registerAdapter<Value>(ValueAdapter());
   final a = await Hive.openBox<dynamic>("docscan");
